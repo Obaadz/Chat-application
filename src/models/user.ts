26fb3e2cf41313255de434b19a98541bd7d7eb08
Obada <obada8411@gmail.com>
2023-02-05 23:17:47 +0200

    Add 'unique' and 'allowNull' fields to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,8 +5,15 @@ const userModel = {
   attributes: {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
 };
 
