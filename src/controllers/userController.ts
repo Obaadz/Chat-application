7622830083061ae48abd95adf35e7f10beb633bd
Obada <obada8411@gmail.com>
2023-02-06 22:58:18 +0200

    Add 'UserController.getAllChatsById' method to retrieve user chats by id

---
+++ src/controllers/userController.ts
@@ -0,0 +1,30 @@
+import { Response } from "express";
+import { JwtAuthRequest } from "../middleware/jwtAuth";
+import { getAllUserChatsById } from "../services/users";
+import { UserFromToken } from "./../types/user";
+
+export default class UserController {
+  static async getAllChatsById(
+    request: JwtAuthRequest<UserFromToken>,
+    response: Response
+  ) {
+    const userIdParams = request.params.id;
+    const user = request.auth;
+
+    try {
+      if (!user?.id)
+        throw new Error(
+          "The token provided is invalid or does not include the necessary 'id' property"
+        );
+
+      if (userIdParams !== user.id)
+        throw new Error(`You don't have access to see user (${userIdParams}) chats`);
+
+      const chats = await getAllUserChatsById(userIdParams);
+
+      response.send({ chats });
+    } catch (err: any) {
+      response.status(401).send(err.message || "Server error");
+    }
+  }
+}
